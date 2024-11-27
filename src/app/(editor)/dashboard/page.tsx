8e40ca89f5dbfc02e2ceb3e2cd0eb2c1456e67cc
Sean Silvius <sean@silvius.me>
2024-11-27 13:08:44 -0800

    starting over with learned lessions

--- src/app/(editor)/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import Editor from "@/components/editor"
-
-export default async function ManageApp() {
-  return(
-    <div className="h-full bg-slate-400 m-h-32 p-8">
-      <Editor  />
-    </div>
-  )
-}
--- src/components/layouts/admin.tsx
+++
@@ -1,8 +0,0 @@
-import { getNavigation } from '@/actions/editor/navigation';
-import EditorLayout from '@/components/editor/EditorLayout';
-
-export default async function EditorPage() {
-  const { navMain } = await getNavigation();
-  
-  return <EditorLayout navigation={navMain} />;
-}
